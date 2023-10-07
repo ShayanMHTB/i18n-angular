7e79199c5123a3eaa77b2c8b86496a4e05399aac
Shayan Mahtabi <shayanmahtabi@Shayans-MBP.box>
2023-10-07 21:01:38 +0200

    Modules and components added to appModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PageComponent } from './page/components/page/page.component';
+import { DashboardComponent } from './dashboard/components/dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path: '', component: PageComponent },
+  { path: 'dashboard', component: DashboardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { PageModule } from './page/page.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    PageModule,
+    DashboardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
